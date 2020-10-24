a56cf41dc10ac74c9a4b130fb8a66bc4347308e6
Eric Hasegawa <eric.hasegawa@mail.utoronto.ca>
2020-10-24 11:41:11 -0400

    Create ReverseLinkedList.py

---
+++ Blind75/ReverseLinkedList.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return None
+        if head.next is None:
+            return head
+        curr = head.next
+        head.next = None
+        prev = head
+        while curr is not None:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
